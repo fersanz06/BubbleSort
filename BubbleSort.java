ca7c04837c7ebfe472e3414a464720df6ca58792
fersanz_06 <luisfernandosanchezbriseno052@gmail.com>
2025-11-19 22:51:51 -0600

    Commit 4

--- BubbleSort.java
+++ BubbleSort.java
@@ -2,13 +2,13 @@ public class BubbleSort {
 
     public static void main(String[] args) {
         int[] numeros = {5, 1, 4, 2, 8};
-
         burbuja(numeros);
+        System.out.println(numeros);
     }
 
     public static void burbuja(int[] arreglo) {
         for (int i = 0; i < arreglo.length; i++) {
-            for (int j = 0; j < arreglo.length - i; j++) {
+            for (int j = 0; j < arreglo.length - i - 1; j++) {
                 if (arreglo[j] > arreglo[j + 1]) {
                     int temp = arreglo[j];
                     arreglo[j] = arreglo[j + 1];
